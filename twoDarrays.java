7a95993d06f0e89e72dea3576061823e4cc05a4c
shirshak_18 <shirshakdixit18@gmail.com>
2024-09-07 15:15:21 +0530

    java 2d arrays

---
+++ twoDarrays.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class twoDarrays {
+    public static void main(String[] args) {
+        Scanner scn  = new Scanner (System.in);
+    int n = scn.nextInt();
+    int m = scn.nextInt();
+    int [][] arr = new int [n][m] ;
+
+    for(int i = 0; i<n; i++) {
+        for(int j = 0; j<m; j++){
+            System.out.print(arr[i][j] + " ");
+        }
+        System.out.println();
+    }
+
+    for(int i = 0; i<n ; i++ ) {
+        for(int j = 0; j<m; j++) {
+            arr[i][j] = scn.nextInt();
+        }
+        
+    }
+        System.out.println("------------------------");
+
+    for(int i = 0; i<n ; i++ ) {
+        for(int j = 0; j<m; j++) {
+            System.out.print(arr[i][j] + " ");
+        }
+        System.out.println();
+    }
+    }
+}
