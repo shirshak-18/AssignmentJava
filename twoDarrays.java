7aae34185da3333012fdb75e062f36cbd858602c
shirshak_18 <shirshakdixit18@gmail.com>
2024-09-08 11:41:49 +0530

    2d arrays

--- twoDarrays.java
+++ twoDarrays.java
@@ -1,32 +1,32 @@
-import java.util.Scanner;
-
+import java.util.Scanner ;
 public class twoDarrays {
     public static void main(String[] args) {
         Scanner scn  = new Scanner (System.in);
-    int n = scn.nextInt();
-    int m = scn.nextInt();
-    int [][] arr = new int [n][m] ;
+        int n = scn.nextInt();
+        int m = scn.nextInt();
+        int [][] arr = new int [n][m];
 
-    for(int i = 0; i<n; i++) {
-        for(int j = 0; j<m; j++){
-            System.out.print(arr[i][j] + " ");
-        }
-        System.out.println();
-    }
-
-    for(int i = 0; i<n ; i++ ) {
-        for(int j = 0; j<m; j++) {
-            arr[i][j] = scn.nextInt();
+        for(int i = 0 ; i<n ; i++ ) {
+            for(int j = 0; j<m; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
         }
         
-    }
-        System.out.println("------------------------");
+        System.out.println("----------------");
 
-    for(int i = 0; i<n ; i++ ) {
-        for(int j = 0; j<m; j++) {
-            System.out.print(arr[i][j] + " ");
+        for(int i = 0 ; i<n ; i++ ) {
+            for(int j = 0; j<m; j++) {
+                arr[i][j] = scn.nextInt();
+            }
         }
-        System.out.println();
-    }
+
+        for(int i = 0 ; i<n ; i++ ) {
+            for(int j = 0; j<m; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
     }
 }
